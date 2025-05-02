85622dec9c23e57bb0a8237d9c3dcbe6d57194c3
Shamaratun <shamaratunmeme@gmail.com>
2025-05-02 22:31:44 +0600

    aaa

--- ProjectBackend/src/main/java/org/isdb/ProjectBackend/config/JwtAuthenticationFilter.java
+++ ProjectBackend/src/main/java/org/isdb/ProjectBackend/config/JwtAuthenticationFilter.java
@@ -1,10 +1,7 @@
 package org.isdb.ProjectBackend.config;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+
 import org.isdb.ProjectBackend.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
