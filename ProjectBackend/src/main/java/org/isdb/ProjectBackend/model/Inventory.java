9dff475dc229757d69709c4082a6cf4990056cca
Shamaratun <shamaratunmeme@gmail.com>
2025-05-04 00:28:20 +0600

    comment out

--- ProjectBackend/src/main/java/org/isdb/ProjectBackend/model/Inventory.java
+++ ProjectBackend/src/main/java/org/isdb/ProjectBackend/model/Inventory.java
@@ -1,30 +1,30 @@
-package org.isdb.ProjectBackend.model;
-
-import java.security.Timestamp;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "Inventory")
-public class Inventory {
-	@Id
-	private Integer inventoryID;
-
-	private Integer quantity;
-
-	@Column(name = "lastUpdated")
-	private Timestamp lastUpdated;
-
-	@ManyToOne
-	@JoinColumn(name = "bookID")
-	private Book book;
-
-	@ManyToOne
-	@JoinColumn(name = "warehouseID")
-	private Warehouse warehouse;
-}
+//package org.isdb.ProjectBackend.model;
+//
+//import java.security.Timestamp;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "Inventory")
+//public class Inventory {
+//	@Id
+//	private Integer inventoryID;
+//
+//	private Integer quantity;
+//
+//	@Column(name = "lastUpdated")
+//	private Timestamp lastUpdated;
+//
+//	@ManyToOne
+//	@JoinColumn(name = "bookID")
+//	private Book book;
+//
+//	@ManyToOne
+//	@JoinColumn(name = "warehouseID")
+//	private Warehouse warehouse;
+//}
--- ProjectBackend/src/main/java/org/isdb/ProjectBackend/model/Payment.java
+++ ProjectBackend/src/main/java/org/isdb/ProjectBackend/model/Payment.java
@@ -1,24 +1,24 @@
-package org.isdb.ProjectBackend.model;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "Payment")
-public class Payment {
-	@Id
-	private Integer paymentID;
-
-	private String paymentMethod;
-	private LocalDateTime paymentDate;
-	private BigDecimal amount;
-	private String transactionStatus;
-
-	@OneToOne(mappedBy = "payment")
-	private Order order;
-}
+//package org.isdb.ProjectBackend.model;
+//
+//import java.math.BigDecimal;
+//import java.time.LocalDateTime;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.OneToOne;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "Payment")
+//public class Payment {
+//	@Id
+//	private Integer paymentID;
+//
+//	private String paymentMethod;
+//	private LocalDateTime paymentDate;
+//	private BigDecimal amount;
+//	private String transactionStatus;
+//
+//	@OneToOne(mappedBy = "payment")
+//	private Order order;
+//}
