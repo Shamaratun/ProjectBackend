06a111203c717c66549d3c4347a1ee59a89f6f74
Shamaratun <shamaratunmeme@gmail.com>
2025-05-04 13:21:52 +0600

    a

--- ProjectBackend/src/main/java/org/isdb/ProjectBackend/model/Author.java
+++ ProjectBackend/src/main/java/org/isdb/ProjectBackend/model/Author.java
@@ -1,31 +1,31 @@
-package org.isdb.ProjectBackend.model;
-
-import java.util.Date;
-import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "Author")
-public class Author {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer authorID;
-	@Column(nullable = false, length = 100)
-	private String name;
-	@Column(nullable = false, length = 100)
-	private String bio;
-	@Column(nullable = false, length = 100)
-	private String country;
-	@Column(nullable = false, length = 100)
-	private Date dob;
-
-	@OneToMany(mappedBy = "author")
-	private List<Books> books;
-}
+//package org.isdb.ProjectBackend.model;
+//
+//import java.util.Date;
+//import java.util.List;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.OneToMany;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "Author")
+//public class Author {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private Integer authorID;
+//	@Column(nullable = false, length = 100)
+//	private String name;
+//	@Column(nullable = false, length = 100)
+//	private String bio;
+//	@Column(nullable = false, length = 100)
+//	private String country;
+//	@Column(nullable = false, length = 100)
+//	private Date dob;
+//
+//	@OneToMany(mappedBy = "author")
+//	private List<Books> books;
+//}
